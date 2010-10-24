184d5034c6be37ec657850b04fcd59c5f00795bb
Paul <shunuk@gmail.com>
2010-10-24 21:06:28 +0100

    Don't output a tag when the string is empty. TODO: any way to improve current method?

--- views/helpers/minify.php
+++ views/helpers/minify.php
@@ -1,12 +1,14 @@
 <?php
 class MinifyHelper extends AppHelper {
 	function js_link($string,$html5=true,$compression_type='gz') {
+		if(empty($string)) return '';
 		if(!$html5) $output = sprintf('<script type="text/javascript" src="/minify/js/%s/%s"></script>',$compression_type,$string);
 		else $output = sprintf('<script src="/minify/js/%s/%s"></script>',$compression_type,$string);
 		return $output;
 	}
 	function css_link($string,$media='',$html5=true,$compression_type='gz') {
-		if(!empty($media)) $media = " media=\".$media.\"";
+		if(empty($string)) return '';
+		if(!empty($media)) $media = " media=\"$media\"";
 		if($html5) $output = sprintf('<link rel="stylesheet" href="/minify/css/%s/%s"%s>',$compression_type,$string,$media);
 		else $output = sprintf('<link rel="stylesheet" type="text/css" href="/minify/css/%s/%s"%s />',$compression_type,$string,$media);
 		return $output;
