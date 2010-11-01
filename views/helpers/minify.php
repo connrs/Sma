5ef34932ec0894ef556c9e213ebb842adbc57b2b
Paul <shunuk@gmail.com>
2010-11-01 21:35:33 +0000

    Added support for complex routing and alternative app directoriesx

--- views/helpers/minify.php
+++ views/helpers/minify.php
@@ -1,16 +1,29 @@
 <?php
+/*
+ *
+ */
 class MinifyHelper extends AppHelper {
+//	var $helpers = array('Html');
+
 	function js_link($string,$html5=true,$compression_type='gz') {
 		if(empty($string)) return '';
-		if(!$html5) $output = sprintf('<script type="text/javascript" src="/minify/js/%s/%s"></script>',$compression_type,$string);
-		else $output = sprintf('<script src="/minify/js/%s/%s"></script>',$compression_type,$string);
+		$url = $this->url(array('plugin'=>'minify','controller'=>'js','action'=>$compression_type,$string));
+		if(!$html5) $output = sprintf('<script type="text/javascript" src="%s"></script>',$url);
+		else $output = sprintf('<script src="%s"></script>',$url);
 		return $output;
 	}
+
 	function css_link($string,$media='',$html5=true,$compression_type='gz') {
 		if(empty($string)) return '';
 		if(!empty($media)) $media = " media=\"$media\"";
-		if($html5) $output = sprintf('<link rel="stylesheet" href="/minify/css/%s/%s"%s>',$compression_type,$string,$media);
-		else $output = sprintf('<link rel="stylesheet" type="text/css" href="/minify/css/%s/%s"%s />',$compression_type,$string,$media);
+		$url = $this->url(array('plugin'=>'minify','controller'=>'css','action'=>$compression_type,$string));
+		if($html5) $output = sprintf('<link rel="stylesheet" href="%s"%s>',$url,$media);
+		else $output = sprintf('<link rel="stylesheet" type="text/css" href="%s"%s />',$url,$media);
 		return $output;
+	}
+	
+	function url($url) {
+		$prefix_array = !empty($this->params['prefix'])?array($this->params['prefix']=>false):array();
+		return parent::url(array_merge($prefix_array,$url));
 	}
 }
